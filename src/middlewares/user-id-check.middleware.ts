6265235b3c7d166d3f446f587d25a6ee87cfe88c
Joao Braga <jfcbxp@gmail.com>
2023-09-01 11:52:49 -0300

    add middleware

---
+++ src/middlewares/user-id-check.middleware.ts
@@ -0,0 +1,16 @@
+import {
+  Injectable,
+  NestMiddleware,
+  BadRequestException,
+} from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class UserIdCheckMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: (error?: NextFunction) => void) {
+    if (isNaN(Number(req.params.id)) || Number(req.params.id) <= 0) {
+      throw new BadRequestException('Id invalido');
+    }
+    next();
+  }
+}
